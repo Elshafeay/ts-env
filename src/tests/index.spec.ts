678c4fcbd34f164541c23e4cd7a1e038fd48e4b0
Mohamed Elshafeay <mohamedelshafeay@gmail.com>
2022-10-01 16:09:59 +0200

    small fix

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -5,7 +5,7 @@ describe('Testing the calc average function', function() {
   it('returns the average for group of numbers', async function() {
     expect(calcAverage(1, 2, 3)).toEqual(2);
     expect(calcAverage(3, 4, 5, 6)).toEqual(4.5);
-    expect(calcAverage(-1, 2, 4, -2)).toEqual(1.5);
+    expect(calcAverage(-1, 2, 4, -2)).toEqual(0.75);
     expect(calcAverage()).toEqual(0);
   });
 
